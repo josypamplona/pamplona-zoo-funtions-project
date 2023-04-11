dd24e18628b4f608cda5e2ef05c9470b3a89d93c
josypamplona <josypamplona@hotmail.com>
2023-04-11 15:32:01 -0300

    requisito 1,2,3

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,12 +1,15 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => data.species.reduce((acc, curr) => {
-  acc[curr.location] = [];
-  const state = data.species.find((location) => state.short ===curr.state)
-  const obj = {
-    location:curr.location,
-  };
-  acc[curr.location].push(obj);
-  return acc
-}); {})
+const getAnimalMap = (options) => {};
+// data.species.reduce((acc, curr) => {
+// acc[curr.location] = [];
+// const state = data.species.find((location) => state.short === curr.state);
+// const obj = {
+//   location: curr.location,
+// };
+// acc[curr.location].push(obj);
+// return acc;
+// }
+
+//  {}
 module.exports = getAnimalMap;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,19 +1,18 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  const ger1 = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-  const ger2 = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-  const ger3 = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-  let retorno = false;
-  if (id === ger1 || id === ger2 || id === ger3) {
-    retorno = true;
-  }
-  return retorno;
+//   const ger1 = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+//   const ger2 = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+//   const ger3 = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+//   let retorno = false;
+//   if (id === ger1 || id === ger2 || id === ger3) {
+//     retorno = true;
+//   }
+//   return retorno;
 };
 
 const getRelatedEmployees = (managerId) => {
-  if (isManager(managerId)) { return data.employees.filter((obj) => obj.firstName && obj.lastName); }
+//   if (isManager(managerId)) { return data.employees.filter((obj) => obj.firstName && obj.lastName); }
 };
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
-
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
