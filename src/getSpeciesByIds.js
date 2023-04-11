02e60a6285c476838e6c09ea3cd95d9a6e1c403a
josypamplona <josypamplona@hotmail.com>
2023-04-11 15:35:03 -0300

    requisito 1,2,3 cons. erro de lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,6 +9,6 @@ const getSpeciesByIds = (...obj) => {
   return empurra;
 };
 
-console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', '0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
