550ef2e84ba56e3ad4d173c9b5455434c78cd0eb
josypamplona <josypamplona@hotmail.com>
2023-04-18 10:47:20 -0300

    numero 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,12 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getEmployeesCoverage = (name) => {
-// const pessoa = data.employees.find((pes) => pes.id === pes.lastName);
-// const achar = pessoa.employees.map((pes) => pes.id )
-// console.log(pessoa);
-//     return pessoa;
-// };
 
-// getEmployeesCoverage("Sharonda Spry");
 };
+  // const mudar = data.employeer.filter((chave) => chave === Object.keys )
+  // const pessoa = data.employees.filter((pes) => pes.lastName || pes.firstName || pes.id);
+  // const mudar
+  // if (pessoa){
+  //   return pessoa.find((obj) => obj.lastName && obj.firstName && obj.id && obj.responsibleFor && obj.location);
+  // }
+  // return achar;
+
+// getEmployeesCoverage('Sharonda');
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,63 +1,28 @@
 const data = require('../data/zoo_data');
 
-// const hora1 = { Tuesday: {
-//   officeHour: 'Open from 8am until 6pm',
-//   exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
-// },
-// };
-// const hora2 = { Wednesday: {
-//   officeHour: 'Open from 8am until 6pm',
-//   exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
-// },
-// };
-// const hora3 = { Thursday: {
-//   officeHour: 'Open from 10am until 8pm',
-//   exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
-// },
-// };
-// const hora4 = { Friday: {
-//   officeHour: 'Open from 10am until 8pm',
-//   exhibition: ['tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'],
-// },
-// };
-// const hora5 = { Saturday: {
-//   officeHour: 'Open from 8am until 10pm',
-//   exhibition: [
-//     'lions', 'tigers',
-//     'bears', 'penguins',
-//     'otters', 'frogs',
-//     'snakes', 'elephants',
-//   ],
-// },
-// };
-// const hora6 = { Sunday: {
-//   officeHour: 'Open from 8am until 8pm',
-//   exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
-// },
-// Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
-// };
+const horaToda = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+};
 
-// // eslint-disable-next-line max-lines-per-function
-const getSchedule = () => {};
-//   const objetTotal = `${hora1}  ${hora2} ${hora3} ${hora4} ${hora5}`;
-//   const animais = data.species.some((obj) => obj.name === scheduleTarget.name);
-//   if (animais) {
-//     return data.species.find((obj) => obj.name === scheduleTarget).availability;
-//   } if (scheduleTarget === 'Tuesday') {
-//     return hora1;
-//   } if (scheduleTarget === 'Wednesday') {
-//     return hora2;
-//   } if (scheduleTarget === 'Thursday') {
-//     return hora3;
-//   } if (scheduleTarget === 'Friday') {
-//     return hora4;
-//   } if (scheduleTarget === 'saturday') {
-//     return hora5;
-//   } if (scheduleTarget === 'Sunday') {
-//     return hora6;
-//   } if (scheduleTarget === (' ')) {
-//     return objetTotal;
-//   }
+const getSchedule = (scheduleTarget) => {
+  const animais = data.species.find((obj) => obj.name === scheduleTarget);
+  if (animais) {
+    return animais.availability;
+  } if (scheduleTarget) {
+    return { [scheduleTarget]: horaToda[scheduleTarget] };
+  } return horaToda;
+};
 
-// console.log(getSchedule(' '));
+console.log(getSchedule(''));
 module.exports = getSchedule;
