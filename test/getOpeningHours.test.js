a423f5376901ffdaeecc4902e497c008a42d28fa
josypamplona <josypamplona@hotmail.com>
2023-04-19 12:52:45 -0300

    13 completa

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,15 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Já que o Zoo está sempre fechado na segunda Para os argumentos Monday e 09:00', () => {
+    expect(getOpeningHours()).toEqual({ Friday: { close: 8, open: 10 },
+      Monday: { close: 0, open: 0 },
+      Saturday: { close: 10, open: 8 },
+      Sunday: { close: 8, open: 8 },
+      Thursday: { close: 8, open: 10 },
+      Tuesday:
+    { close: 6, open: 8 },
+      Wednesday: { close: 6, open: 8 },
+    });
+  });
+});
